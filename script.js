3799baf86d54aa530e58f49928c977becd4dc350
Kerashane <gwenaelle.b.71@gmail.com>
2024-12-02 08:15:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let cle = Object.keys(SAE)
+let description = []
+let competence = []
+let sae = ""
+
+for(let num = 0; num<cle.length; num++) {
+    description.push(SAE[cle[num]].titre)
+    competence.push(SAE[cle[num]].compétences.join(" "))
+}
+
+for(let cook = 0; cook<cle.length; cook++) {
+    sae += '<a href=""><div class="sae"><h2>'+cle[cook]+'</h2><p>'+description[cook]+'</p><div class="competences">'+competence[cook]+'</div></div></a>'
+}
+
+document.querySelector(".zone-sae").innerHTML=sae
